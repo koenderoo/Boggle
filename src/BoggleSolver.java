e1dde99baee2426013ca16dd94183ac689c399f7
Koen <kdroo@MacBook-Pro-van-Koen.local>
2018-02-21 22:43:51 +0100

    Initial commit

---
+++ src/BoggleSolver.java
@@ -0,0 +1,45 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.TST;
+
+public class BoggleSolver
+{
+    private TST[] searchTries;
+
+    // Initializes the data structure using the given array of strings as the dictionary.
+    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+    public BoggleSolver(String[] dictionary) {
+        searchTries = new TST[26]; // 26 TST's, voor elke startletter 1
+        // laad alle woorden uit dictionary in de searchTries
+        for (int i = 0; i < dictionary.length; i++) {
+            String word = dictionary[i];
+            searchTries[word.charAt(0)].put(word, i);
+        }
+    }
+
+    // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        return null;
+    }
+
+    // Returns the score of the given word if it is in the dictionary, zero otherwise.
+    // (You can assume the word contains only the uppercase letters A through Z.)
+    public int scoreOf(String word) {
+        return -1;
+    }
+
+
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
+    }
+}
